cd590623ce8fe2452d6d68e5c906da9928191451
dacicelmir <elmir.dacic@udg.edu.me>
2025-12-04 09:27:12 +0100

    Add files via upload

---
+++ projekat5/CircleCollider.java
@@ -0,0 +1,10 @@
+package game;
+
+public class CircleCollider implements Collidable {
+	private int cx, cy, radius;
+
+	public boolean intersects(Collidable other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
